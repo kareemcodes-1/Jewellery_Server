bc18caa16c8e00f1ae66318271976a1e205d5d2d
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-21 13:34:46 +0100

    mongodb env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export async function connectDB(){
     try {
-        const connect = await mongoose.connect('mongodb://0.0.0.0/jewellery_store');
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
         const host = connect.connection.host;
         console.log(`Connected successfully on ${host}`);
     } catch (error) {
